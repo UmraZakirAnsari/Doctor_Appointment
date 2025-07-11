87d3212e5892947edb5ab162bcf370750fe8b98b
Umra Zakir Ansari <ansariumrazakir@gmail.com>
2025-07-11 22:59:37 +0530

    Fix casing issue in AppContext imports

--- frontend/src/component/TopDoctor.jsx
+++ frontend/src/component/TopDoctor.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useContext } from 'react';
 
 import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/Appcontext';
+import { AppContext } from '../context/AppContext';
 
 const TopDoctor = () => {
     const navigate=useNavigate();
